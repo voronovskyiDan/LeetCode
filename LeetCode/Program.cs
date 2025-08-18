e3e1e44c4cbea3e2d31511d499e676e20c860f19
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-18 14:50:59 +0200

    Solution of the 128

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,15 +7,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        IList<IList<string>> res = GroupAnagram.Solution(new string[] { "abets", "bead", "remain", "betas", "abed", "baste", "airline", "leading", "beast", "dealing", "beats", "airmen", "marine", "single", "bade", "aligned" });
+        int res = LongestConsecutiveSequence.Solution(new int[] { 100, 4, 200, 1, 3, 2 });
 
-        foreach(var group in res)
-        {
-            foreach(var an in group)
-            {
-                Console.Write(an + " ");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine(res);
     }
 }
---
+++ LeetCode/mid/s128.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mid
+{
+    class LongestConsecutiveSequence
+    {
+        public static int Solution(int[] nums)
+        {
+            HashSet<int> values = new HashSet<int>(nums);
+            int max = 0;
+
+            foreach(var num in values)
+            {
+                if (!values.Contains(num - 1))
+                {
+                    int count = 0;
+                    int currnet = num;
+
+                    while (values.Contains(currnet))
+                    {
+                        currnet++;
+                        count++;
+                    }
+
+                    max = Math.Max(max, count);
+                }
+            }
+
+            return max;
+        }
+    }
+}
