310a9179ca1c7f4b79bbaa7aebd71c34194e85d7
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-18 23:34:37 +0200

    Solution of the 20

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,18 +7,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int[][] res = InsertInterval.Solution(new int[][] {
-                new int[] { 1, 3 },
-                new int[] { 6, 9 },
-            }, new int[] { 2, 5 });
-
-        foreach(var group in res)
-        {
-            foreach(var it in group)
-            {
-                Console.Write(it + " ");
-            }
-            Console.WriteLine();
-        }
+        bool res = ValidParentheses.Solution("([])");
+        Console.WriteLine(res);
     }
 }
---
+++ LeetCode/easy/s20.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    class ValidParentheses
+    {
+        public static bool Solution(string s)
+        {
+            Stack<char> stack = new();
+            foreach(char ch in s)
+            {
+                if(ch == '{' || ch == '[' || ch == '(')
+                {
+                    stack.Push(ch);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char val = stack.Pop();
+                    if (ch == ']' && val != '[')
+                        return false;
+                    if(ch == ')' && val != '(')
+                        return false;
+                    if (ch == '}' && val != '{')
+                        return false;
+
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
