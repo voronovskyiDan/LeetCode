2030702a5348d3a28956decd09af56a9f09b8af5
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-19 17:48:28 +0200

    Solution of the 226

---
+++ LeetCode/easy/s226.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.supportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    class InvertBinaryTree
+    {
+        public TreeNode Solution(TreeNode root)
+        {
+            if (root == null) return null;
+
+            TreeNode node = root.left;
+            root.left = root.right;
+            root.right = node;
+
+            Solution(root.left);
+            Solution(root.right);
+
+            return root;
+        }
+    }
+}
