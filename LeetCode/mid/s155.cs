bc205d5b22295b9da1a4adba1a4a6cd15e176d3b
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-19 15:56:26 +0200

    Solution of the 155

---
+++ LeetCode/mid/s155.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mid
+{
+    class MinStack
+    {
+        private LinkedList<MinNode> data;
+
+        public MinStack()
+        {
+            data = new();
+
+        }
+
+        public void Push(int val)
+        {
+            int newMin = data.Count == 0 ? val : Math.Min(val, data.Last.Value.min);
+            data.AddLast(new MinNode(val, newMin));
+        }
+
+        public void Pop()
+        {
+            data.RemoveLast();
+        }
+
+        public int Top()
+        {
+            return data.Last.Value.val;
+        }
+
+        public int GetMin()
+        {
+            return data.Last.Value.min;
+        }
+    }
+    class MinNode
+    {
+        public MinNode(int val, int min)
+        {
+            this.val = val;
+            this.min = min;
+        }
+
+        public int val { get; set; }
+        public int min { get; set; }
+    }
+}
