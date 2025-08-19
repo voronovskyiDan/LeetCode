a70556c472b8994723dcc94193606c57ea0de386
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-19 16:51:11 +0200

    Solution of the 100

---
+++ LeetCode/easy/s100.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.supportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    class SameTree
+    {
+        public bool Solution(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if((p == null && q != null) || (p != null && q == null))
+                return false;
+
+            if (p.val != q.val)
+                return false;
+
+            return Solution(p.left,q.left) && Solution(p.right,q.right);
+        }
+    }
+}
