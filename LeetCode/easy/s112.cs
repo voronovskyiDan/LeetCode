fc45ec61a67f66ccac5d41812272e59cfe8d559f
Sparrow <voronovskyy.danylo@gmail.com>
2025-08-19 18:05:07 +0200

    Solution of the 112

---
+++ LeetCode/easy/s112.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.supportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    class PathSum
+    {
+        public static bool Solution(TreeNode root, int targetSum)
+        {
+            if (root == null || targetSum < 0)
+                return false;
+            if (targetSum - root.val == 0 && root.left == null && root.right == null)
+                return true;
+            
+            return Solution(root.left, targetSum-root.val) || Solution(root.right, targetSum - root.val);
+        }
+    }
+}
