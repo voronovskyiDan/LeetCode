169115194f5796c30a3d1bb8b819562555257870
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-06 13:56:29 +0300

    Solution of the 530

---
+++ LeetCode/easy/s530.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.supportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    class MinimumAbsoluteDifferenceInBST
+    {
+        private int min = int.MaxValue;
+        private int? prev = null;
+
+        public int Solution(TreeNode root)
+        {
+            if (root == null) return min;
+
+            Solution(root.left);
+
+            if(prev != null)
+            {
+                min = Math.Min(min, root.val - prev.Value);
+            }
+            prev = root.val;
+
+            Solution(root.right);
+
+            return min;
+        }
+    }
+}
