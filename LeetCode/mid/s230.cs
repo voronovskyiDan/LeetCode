34c6bfc962dfde48047d1d3e8324dc8152ac21bf
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-06 15:18:24 +0300

    Solution of the 98

--- LeetCode/mid/s230.cs
+++ LeetCode/mid/s230.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode.easy
+namespace LeetCode.mid
 {
     internal class KthSmallestElementInBST
     {
---
+++ LeetCode/mid/s98.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.supportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mid
+{
+    internal class ValidateBinarySearchTree
+    {
+        public bool Solution(TreeNode root)
+        {
+            return IsValidBST(root, long.MinValue, long.MaxValue);
+        }
+        private bool IsValidBST(TreeNode node, long min, long max)
+        {
+            if (node == null)
+                return true;
+            if (node.val <= min || node.val >= max)
+                return false;
+            return IsValidBST(node.left, min, node.val) && IsValidBST(node.right, node.val, max);
+        }
+    }
+}
