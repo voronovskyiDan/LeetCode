1bec4a53b6822f53f7253e1b8e76bd610a9b72bc
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-13 14:04:34 +0300

    Solution of the 190

---
+++ LeetCode/easy/s190.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    internal class ReverseBits
+    {
+        public static int Solution(int n)
+        {
+            uint x = unchecked((uint)n);
+            uint result = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                result <<= 1;
+                result |= x & 1;
+                x >>= 1;
+            }
+
+            return unchecked((int)result);
+        }
+    }
+}
