adfbc86a3e965e059bbc2a24b5e8cb9f5713a64a
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-17 15:08:05 +0300

    Solution of the 137 and 260

---
+++ LeetCode/mid/s137.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mid
+{
+    internal class SingleNumnberII
+    {
+        public static int Solution(int[] nums)
+        {
+            int ones = 0, twos = 0;
+            foreach (int num in nums)
+            {
+                twos |= ones & num;
+                ones ^= num;
+                int threes = ones & twos;
+                ones &= ~threes;
+                twos &= ~threes;
+            }
+            return ones;
+        }
+    }
+}
---
+++ LeetCode/mid/s260.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.mid
+{
+    internal class SingleNumberIII
+    {
+        public static int[] Solution(int[] nums)
+        {
+            int xor = 0;
+            foreach (int num in nums)
+            {
+                xor ^= num;
+            }
+
+            int diffBit = xor & -xor;
+            int[] result = new int[2];
+            foreach (int num in nums)
+            {
+                if ((num & diffBit) == 0)
+                {
+                    result[0] ^= num;
+                }
+                else
+                {
+                    result[1] ^= num;
+                }
+            }
+            return result;
+        }
+    }
+}
