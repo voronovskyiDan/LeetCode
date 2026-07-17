01e1a5f23f08cad28a2570dfbe45cbe62481df0e
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-17 13:28:51 +0300

    Solution of the 191

---
+++ LeetCode/easy/s191.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    internal class NumberOf1Bits
+    {
+        public static int Solution(int n) {
+            int count = 0;
+            while (n != 0)
+            {
+                if ((n & 1) == 1)
+                    count++;
+                n = n >>> 1;
+            }
+            return count;
+        }
+    }
+}
