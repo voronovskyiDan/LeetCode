7650e064f511f3f76c180b316ed81352e37a4933
Sparrow <voronovskyy.danylo@gmail.com>
2026-07-17 12:50:58 +0300

    Solution of the 136

---
+++ LeetCode/easy/s136.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.easy
+{
+    internal class SingleNumber
+    {
+        public static int Solution(int[] nums)
+        {
+            int result = 0;
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
